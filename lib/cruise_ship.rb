f9b207661d27ecdc9081c855776fe6415a0d55b7
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-24 02:14:33 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,12 +6,12 @@ suite_e: "Crumpet the Elf"
 }
 
 def select_winner(passengers)
-  winner  = ""
+    winner = ""
   passengers.each do |suite, name|
-    if suite == :suite_a && name.stat_with?("A")
+    if suite == :suite_a && name.start_with?("A")
       winner = name
     end
   end
-  
- winner
+ 
+  winner  
 end
